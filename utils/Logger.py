a2a7cb3ea50b70a70ee4e71de6fb1337475deda4
diff3 <magnus@entropy.nu>
2024-07-19 16:20:00 +0200

    some changs to logging

--- utils/Logger.py
+++ utils/Logger.py
@@ -52,39 +52,56 @@ class Logger:
         return f'{color.value}{label}{Style.RESET_ALL} {date} {msg}'
 
     @staticmethod
+    def add_to_log(msg, debugLevel):
+        file = config['Logging']['log_file']
+
+        date = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+        formatted_msg = f'[{debugLevel}] [{date}] {msg}'
+
+        with open(f'logs/{file}', 'a') as log_file:
+            log_file.write(formatted_msg + '\n')
+    
+    @staticmethod
     def debug(msg):
         if Logger._should_log(DebugLevel.DEBUG):
             print(Logger._colorize_message('[DEBUG]', DebugColorLevel.DEBUG, msg))
+            Logger.add_to_log(msg, 'DEBUG')
 
     @staticmethod
     def warning(msg):
         if Logger._should_log(DebugLevel.WARNING):
             print(Logger._colorize_message('[WARNING]', DebugColorLevel.WARNING, msg))
+            Logger.add_to_log(msg, 'WARNING')
 
     @staticmethod
     def error(msg):
         if Logger._should_log(DebugLevel.ERROR):
             print(Logger._colorize_message('[ERROR]', DebugColorLevel.ERROR, msg))
+            Logger.add_to_log(msg, 'ERROR')
 
     @staticmethod
     def info(msg, end='\n'):
         if Logger._should_log(DebugLevel.INFO):
-            print(Logger._colorize_message('[INFO]', DebugColorLevel.INFO, msg), end=end)
+            print(Logger._colorize_message('[INFO]', DebugColorLevel.INFO, msg))
+            Logger.add_to_log(msg, 'INFO')
 
     @staticmethod
     def success(msg):
         if Logger._should_log(DebugLevel.SUCCESS):
             print(Logger._colorize_message('[SUCCESS]', DebugColorLevel.SUCCESS, msg))
+            Logger.add_to_log(msg, 'SUCCESS')
 
     @staticmethod
     def anticheat(msg):
         if Logger._should_log(DebugLevel.ANTICHEAT):
             print(Logger._colorize_message('[ANTICHEAT]', DebugColorLevel.ANTICHEAT, msg))
+            Logger.add_to_log(msg, 'ANTICHEAT')
 
     @staticmethod
     def script(msg):
         if Logger._should_log(DebugLevel.SCRIPT):
             print(Logger._colorize_message('[SCRIPT]', DebugColorLevel.SCRIPT, msg))
+            Logger.add_to_log(msg, 'SCRIPT')
 
     @staticmethod
     def package(data_str, logging_mask=None):
@@ -101,6 +118,7 @@ class Logger:
                 formatted_message += f'   {key}={value_str},\n'
             formatted_message = formatted_message.rstrip(',\n') + '\n)'
             print(Logger._colorize_message('[PACKAGE]', DebugColorLevel.DEBUG, formatted_message))
+            Logger.add_to_log(formatted_message, 'PACKAGE')
 
     # Additional methods
 
